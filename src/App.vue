4a1da89325e6e9a046d0ecc6f2661a20664a7847
YKavee <ykaveesenanayake@gmail.com>
2019-07-26 09:46:55 -0500

    Add:Navigation Bar for user acc

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <app-nav></app-nav>
+  </div>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+
+  export default
+  {
+    components:{
+
+      'app-nav':Navbar
+
+    }
+  }
+
+</script>
+
+
+<style>
+
+</style>

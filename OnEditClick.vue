8903b6151aefa93420f869ce3416968c0bf47994
Shyamali Ekanayaka <52402034+ShammiEkanayaka@users.noreply.github.com>
2019-08-17 21:10:20 +0530

    OnEditClick added

---
+++ OnEditClick.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <!-- bind the onEditClicked() function to the edit event with the v-on directive -->
+    <user-list
+      :users="users"
+      v-on:edit="onEditClicked"
+    ></user-list>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+ 
+  methods: {
+    
+    onEditClicked (user) {
+
+       this.userInForm = { user }
+    }
+  }
+}
+</script>

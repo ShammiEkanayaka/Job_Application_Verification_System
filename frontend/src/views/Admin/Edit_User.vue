5f3d974d818c140aa7c675d2d6706258da12c1d3
ChanakaDushmantha <2016sp314@gmail.com>
2019-08-17 21:48:20 +0530

    image preview : ok

--- frontend/src/views/Admin/Edit_User.vue
+++ frontend/src/views/Admin/Edit_User.vue
@@ -10,7 +10,9 @@
                 <div class="card-body">
                     <h6 class="card-subtitle text-muted">Profile picture</h6>
                 </div>
-                <img v-bind:src="'http://localhost:8000/Profile_Pic/'+user.image" alt="Card image">
+                <img v-if="!user.image" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22318%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20318%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_158bd1d28ef%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A16pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_158bd1d28ef%22%3E%3Crect%20width%3D%22318%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22129.359375%22%20y%3D%2297.35%22%3EImage%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image">
+                <img v-else-if="!imaging" v-bind:src="'http://localhost:8000/Profile_Pic/'+user.image" alt="Card image">
+                <img v-else v-bind:src="user.image" alt="Card image">
                 <div class="card-footer text-muted">
                     <div class="form-group mb-1">
                         <div class="input-group">
@@ -162,8 +164,8 @@ export default {
      data(){
          return {
              user:{
-
-             }
+             },
+             imaging : false
          }
      },
      methods:{
@@ -186,6 +188,7 @@ export default {
             filereader.onload = (e)=>{
                 this.user.image = e.target.result;
             }
+            this.imaging = true;
         }
      }
  }

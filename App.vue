78183ff2e1124dbc8f05449534c77c40e2089fed
Shyamali Ekanayaka <52402034+ShammiEkanayaka@users.noreply.github.com>
2019-08-17 20:47:21 +0530

    Added app.vue file.
    App.vue is a Single File Component. It contains 3 chunks of code: HTML, CSS and JavaScript.

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <app-nav></app-nav>
+    
+  </div>
+</template>
+
+<script>
+//import Home from './components/Home.vue'
+ // import HomeSecurity from './components2/HomeSecurity.vue'
+  export default
+  {
+    components:{
+      //'app-home':Home,
+      //'userAccount-Security': HomeSecurity
+    }
+  }
+
+</script>
+
+<style>
+
+</style>

68cdf6ea366f247e9138f6292f849a2743dfbe5c
Shyamali Ekanayaka <52402034+ShammiEkanayaka@users.noreply.github.com>
2019-08-17 21:25:59 +0530

    Add files via upload

---
+++ RemoveUserDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <table class="table table-hover user-table">
+
+    <tbody>
+       <!-- <tr v-for="user in users" track-by="id" v-on:click.prevent="onEdit"> 
+         <td><a href="#" v-on:click.prevent.stop="onRemove(user.id)">remove</a></td>
+      </tr>
+      -->
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: ['users'],
+  methods: {
+
+    onRemove (userId) {
+      this.$emit('remove', userId)
+    }
+  }
+}
+</script>

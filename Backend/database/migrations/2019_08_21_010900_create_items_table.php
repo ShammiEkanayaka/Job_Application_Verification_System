5074e8d1277dd3081e0efcc6678a9f1438848b12
YKavee <ykaveesenanayake@gmail.com>
2019-08-20 22:28:36 -0500

    created laravel backend initial part

---
+++ Backend/database/migrations/2019_08_21_010900_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->text('name');
+            $table->integer('reg_no');
+            $table->String('email');
+            $table->integer('phone_no');
+            $table->String('university');
+            $table->String('faculty');
+            $table->String('degree');
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}

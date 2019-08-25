efce29253889dafc5833e94e4203073162418941
ChanakaDushmantha <2016sp314@gmail.com>
2019-08-26 01:33:59 +0530

    add new dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import vueResource from 'vue-resource'
+import vueResource from 'vue-resource';
+import { BFormFile } from 'bootstrap-vue';
+import { FormCheckboxPlugin } from 'bootstrap-vue';
 
+
+Vue.use(FormCheckboxPlugin);
+Vue.component('b-form-file', BFormFile);
 Vue.use(vueResource);
 Vue.config.productionTip = false;
 

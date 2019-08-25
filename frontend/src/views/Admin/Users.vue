c9e557d502decea82d48411f88fb3664aec24e0f
ChanakaDushmantha <2016sp314@gmail.com>
2019-08-26 01:30:06 +0530

    add: scroll to top

--- frontend/src/views/Admin/Users.vue
+++ frontend/src/views/Admin/Users.vue
@@ -50,6 +50,7 @@ export default {
     } 
   },
   created(){
+    this.scrollToTop();
     this.$http.get("http://localhost:8000/api/getUser")
     .then(function (response){
       this.Users = response.body.allUser;
@@ -66,6 +67,9 @@ export default {
         this.Users.splice(event.target.title,1)
       })
     },
+    scrollToTop() {
+                window.scrollTo(0,0);
+           },
     filterBy(list, value){
       return list.filter(function(user){
         return user.index.indexOf(value) > -1;

1548ee00eb37ad554b48c8b53cffc46e42584da7
ChanakaDushmantha <2016sp314@gmail.com>
2019-10-06 18:24:48 +0530

    Carousel

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -6,19 +6,23 @@
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src=".\HomeImage\01.jpeg" class="d-block w-100" alt="..." />
+          <img src=".\HomeImage\01.jpg" class="d-block w-100" alt="..." />
         </div>
         <div class="carousel-item">
-          <img src=".\HomeImage\02.jpg" class="d-block w-100" alt="..." />
+          <img src=".\HomeImage\02.png" class="d-block w-100" alt="..." />
         </div>
         <div class="carousel-item">
           <img src=".\HomeImage\03.jpg" class="d-block w-100" alt="..." />
         </div>
         <div class="carousel-item">
-          <img src=".\HomeImage\04.jpg" class="d-block w-100" alt="..." />
+          <img src=".\HomeImage\04.png" class="d-block w-100" alt="..." />
+        </div>
+        <div class="carousel-item">
+          <img src=".\HomeImage\05.jpg" class="d-block w-100" alt="..." />
         </div>
       </div>
       <a
